46e125607420cd01b42a4eaa85c256573267c085
Suzuki Kaito <tanichi.u.n.20@gmail.com>
2021-03-14 21:42:16 +0900

    それぞれの個別のSquareに現在の値を伝えるように

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ class Square extends React.Component {
     }
 
     renderSquare(i) {
-        return <Square value={i} />;
+        return <Square value={this.state.squares[i]} />;
     }
 
     render() {

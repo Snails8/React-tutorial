fed9bd644d5a00423c1497a665a50b37ba977c55
Suzuki Kaito <tanichi.u.n.20@gmail.com>
2021-03-14 21:26:46 +0900

    アロー関数構文で書き換え

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import './index.css';
 class Square extends React.Component {
     render() {
         return (
-            <button className="square" onclick=
-        {function() {alert('click'); }}>
+            <button className="square" onClick={() => alert('click')}>
                 {this.props.value}
             </button>
         )

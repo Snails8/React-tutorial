2fadcdf64f56cd444ec74c326895cf3203418f01
Suzuki Kaito <tanichi.u.n.20@gmail.com>
2021-03-16 08:48:55 +0900

    Square を関数コンポーネントに書き換え

--- src/index.js
+++ src/index.js
@@ -2,32 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    constructor(props, ) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-        };
-    }
-
-    renderSquare(i) {
-        return (
-          <Square 
-            value={this.state.squares[i]} 
-            onClick={() => this.handleClick(i)}
-            />
-        );
-    }
-
-    render() {
-        return (
-            <button className="square" 
-                onClick={() => this.setState({value: 'X'})}
-            >
-                {this.props.value}
-            </button>
-        )
-    };
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    ); 
 }
 
 class Board extends React.Component {
